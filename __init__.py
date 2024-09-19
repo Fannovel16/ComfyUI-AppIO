197978dfbb83c6a1585362c981b17cea85b7ee6c
H.D.Tài <tai17082006.tk@gmail.com>
2024-09-19 16:38:39 +0700

    Add resize node

--- __init__.py
+++ __init__.py
@@ -51,7 +51,7 @@ class AppIO_ImageInput:
                     {"image": (sorted(files), {"image_upload": True})},
                 }
 
-    CATEGORY = "image"
+    CATEGORY = "AppIO"
 
     RETURN_TYPES = ("IMAGE", "MASK")
     FUNCTION = "execute"
@@ -141,11 +141,80 @@ class AppIO_ImageOutput:
 
     def execute(self, image):
         return (image,)
+
+# Sauce: https://github.com/bronkula/comfyui-fitsize/blob/main/nodes.py
+# Make a copy as the original node isn't compatiable with https://github.com/pydn/ComfyUI-to-Python-Extension
+class AppIO_FitResizeImage:
+    @classmethod
+    def INPUT_TYPES(s):
+        return {
+            "required": {
+                "image": ("IMAGE",),
+                "max_size": ("INT", {"default": 768, "step": 8}),
+                "resampling": (["lanczos", "nearest", "bilinear", "bicubic"],),
+                "upscale": (["false", "true"],)
+            }
+        }
+
+    RETURN_TYPES = ("IMAGE","INT","INT","FLOAT")
+    RETURN_NAMES = ("Image","Fit Width", "Fit Height", "Aspect Ratio")
+    FUNCTION = "fit_resize_image"
+
+    CATEGORY = "AppIO"
+
+    def tensor2pil(self, image):
+        return Image.fromarray(np.clip(255. * image.cpu().numpy().squeeze(), 0, 255).astype(np.uint8))
+    
+    def get_max_size(self, width, height, max, upscale="false"):
+        def octal_sizes(width, height):
+            octalwidth = width if width % 8 == 0 else width + (8 - width % 8)
+            octalheight = height if height % 8 == 0 else height + (8 - height % 8)
+            return (octalwidth, octalheight)
+        
+        aspect_ratio = width / height
+
+        fit_width = max
+        fit_height = max
+
+        if upscale == "false" and width <= max and height <= max:
+            return (width, height, aspect_ratio)
+        
+        if aspect_ratio > 1:
+            fit_height = int(max / aspect_ratio)
+        else:
+            fit_width = int(max * aspect_ratio)
+        
+        new_width, new_height = octal_sizes(fit_width, fit_height)
+        return (new_width, new_height, aspect_ratio)
+    
+    def pil2tensor(self, image):
+        return torch.from_numpy(np.array(image).astype(np.float32) / 255.0).unsqueeze(0)
+    
+    def get_image_size(self, IMAGE) -> tuple[int, int]:
+        samples = IMAGE.movedim(-1, 1)
+        size = samples.shape[3], samples.shape[2]
+        return size
+
+    def fit_resize_image(self, image, max_size=768, resampling="bicubic", upscale="false"):
+        resample_filters = {
+            'nearest': 0,
+            'lanczos': 1,
+            'bilinear': 2,
+            'bicubic': 3,
+        }
+        size = self.get_image_size(image)
+        img = self.tensor2pil(image)
+
+        new_width, new_height, aspect_ratio = self.get_max_size(size[0], size[1], max_size, upscale)
+        resized_image = img.resize((new_width, new_height), resample=Image.Resampling(resample_filters[resampling]))
+
+        return (self.pil2tensor(resized_image),new_width,new_height,aspect_ratio)
     
 NODE_CLASS_MAPPINGS = {
     "AppIO_StringInput": AppIO_StringInput,
     "AppIO_ImageInput": AppIO_ImageInput,
     "AppIO_StringOutput": AppIO_StringOutput,
     "AppIO_ImageOutput": AppIO_ImageOutput,
-    "AppIO_IntegerInput": AppIO_IntegerInput
+    "AppIO_IntegerInput": AppIO_IntegerInput,
+    "AppIO_FitResizeImage": AppIO_FitResizeImage
 }
